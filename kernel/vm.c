93d55921891d85cecb30e77b83dcda08026e5033
Your Name <you@example.com>
2025-10-17 14:49:51 +0800

    Detecting which pages have been accessed

--- kernel/vm.c
+++ kernel/vm.c
@@ -462,3 +462,19 @@ void vmprint(pagetable_t pagetable, uint64 depth)
     }
   }
 }
+
+int vm_pgaccess(pagetable_t pagetable, uint64 va)
+{
+  pte_t *pte;
+  if(va >= MAXVA)
+    return 0;
+
+  pte = walk(pagetable, va, 0);
+  if(pte == 0)
+    return 0;
+  if((*pte & PTE_A) != 0) {
+    *pte = *pte & (~PTE_A);
+    return 1;
+  }
+  return 0; 
+}
